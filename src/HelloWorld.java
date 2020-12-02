5bf29034628578791acc134f5dd57792dd5c0af3
Vladislav Uhwatov <vlad.uhwatov@yandex.ru>
2020-12-02 21:33:45 +0200

    Acquaintance to Input and Output.

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println("Destroy this World, " + name);
+    }
+}
